947e04afb597c6814f5be5006919ade4990ddf6a
Taner-Y-Banth <62570956+Taner-Y-Banth@users.noreply.github.com>
2022-04-14 17:36:56 -0700

    init

---
+++ discord-sender/index.js
@@ -0,0 +1,42 @@
+import minimist from 'minimist';
+import { NstrumentaClient } from 'nstrumenta';
+import ws from 'ws';
+import Discord from 'discord.js';
+
+const argv = minimist(process.argv.slice(2));
+const wsUrl = argv.wsUrl;
+const nstClient = new NstrumentaClient();
+
+const prefix = '-'
+const bot = new Discord.Client();
+const token = 'Nzg5NzMwOTI1OTQzODQ4OTkx.X92UPQ.ViiWLBoM2aJJ00cxaXMHgwDGW08'
+
+bot.login(token)
+
+bot.on('message', async (msg) => {
+    //if our message doesnt start with our defined prefix, dont go any further into function
+    if (!msg.content.startsWith(prefix)) {
+        return
+    }
+
+    //slices off prefix from our message, then trims extra whitespace, then returns our array of words from the message
+    const args = msg.content.slice(prefix.length).trim().split(' ')
+
+    //splits off the first word from the array, which will be our command
+    const command = args.shift().toLowerCase()
+
+    if (command === 'personal') {
+        msg.reply('https://www.youtube.com/watch?v=miZHa7ZC6Z0')
+        nstClient.send('personal', 'personal')
+    }
+    if (command === 'altpc') {
+        msg.reply('https://www.youtube.com/watch?v=e_DqV1xdf-Y')
+        nstClient.send('altpc', 'altpc')
+    }
+})
+
+nstClient.addListener("open", () => {
+    console.log("websocket opened successfully");
+});
+
+nstClient.connect({ wsUrl, nodeWebSocket: ws });
---
+++ receiver/index.js
@@ -0,0 +1,31 @@
+import minimist from 'minimist';
+import { NstrumentaClient } from 'nstrumenta';
+import ws from 'ws';
+import { $ } from 'zx';
+
+const argv = minimist(process.argv.slice(2));
+const wsUrl = argv.wsUrl;
+const personalMac = argv.personalMac
+const altMac = argv.altpcMac
+const nstClient = new NstrumentaClient();
+
+nstClient.addListener("open", () => {
+
+    console.log("websocket opened successfully");
+
+    nstClient.addSubscription('packets', (mac) => {
+        $`wakeonlan ${mac}`
+    });
+    nstClient.addSubscription('personal', (message) => {
+        if (message == 'personal') {
+            $`wakeonlan ${personalMac}`
+        };
+    });
+    nstClient.addSubscription('altpc', (message) => {
+        if (message == 'altpc') {
+            $`wakeonlan ${altMac}`
+        }
+    });
+});
+
+nstClient.connect({ wsUrl, nodeWebSocket: ws });
