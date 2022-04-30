9b055ec7b26189cc39818430013606912881dcde
Taner-Y-Banth <62570956+Taner-Y-Banth@users.noreply.github.com>
2022-04-30 15:49:46 -0700

    removes approaching

--- discord-sender/index.js
+++ discord-sender/index.js
@@ -11,35 +11,28 @@ const token = argv.token
 const prefix = '-'
 const bot = new Discord.Client();
 
-bot.login(token)
+bot.login(token);
 
 bot.on('message', async (msg) => {
     //if our message doesnt start with our defined prefix, dont go any further into function
     if (!msg.content.startsWith(prefix)) {
         return
-    }
+    };
 
     //slices off prefix from our message, then trims extra whitespace, then returns our array of words from the message
-    const args = msg.content.slice(prefix.length).trim().split(' ')
+    const args = msg.content.slice(prefix.length).trim().split(' ');
 
     //splits off the first word from the array, which will be our command
-    const command = args.shift().toLowerCase()
+    const command = args.shift().toLowerCase();
 
     if (command === 'personal') {
-        msg.reply('https://www.youtube.com/watch?v=miZHa7ZC6Z0')
-        nstClient.send('personal', 'personal')
-    }
+        msg.reply('https://www.youtube.com/watch?v=miZHa7ZC6Z0');
+        nstClient.send('personal', 'personal');
+    };
     if (command === 'altpc') {
-        msg.reply('https://www.youtube.com/watch?v=e_DqV1xdf-Y')
-        nstClient.send('altpc', 'altpc')
-    }
-})
-
-nstClient.addListener("open", () => {
-    console.log("websocket opened successfully");
-    nstClient.addSubscription('alert', (msg)=>{
-        msg.reply('***APPROACHING*** @TanerYBanth')
-    });
+        msg.reply('https://www.youtube.com/watch?v=e_DqV1xdf-Y');
+        nstClient.send('altpc', 'altpc');
+    };
 });
 
 nstClient.connect({ wsUrl, nodeWebSocket: ws });
